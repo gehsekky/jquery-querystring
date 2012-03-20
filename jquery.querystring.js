44c22721bc741221ab55a77d7e3dc8ec56e41124
andy chung <chung.andy@gmail.com>
2012-03-20 00:39:54 -0400

    removed extra linebreak

--- jquery.querystring.js
+++ jquery.querystring.js
@@ -1,9 +1,7 @@
 (function($){
 	$.extend({
 		querystring: function(name) {
-			var match = RegExp('[?&]' + name + '=([^&]*)')
-	        .exec(window.location.search);
-	
+			var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
 			return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
 		}
 	});	
